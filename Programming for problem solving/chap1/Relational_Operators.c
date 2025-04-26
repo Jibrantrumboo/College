5f92e59303491f39f1c340acfc18e9d4dd64f553
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 17:00:43 +0530

    Create Relational_Operators.c

---
+++ Programming for problem solving/chap1/Relational_Operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10, b = 5, c;
+  int *ptr = &a;
+
+// Relational Operators
+printf("Relational Operators:\n");
+printf("a == b: %d\n",a == b);
+printf("a != b: %d\n",a != b);
+printf("a > b: %d\n",a > b);
+printf("a < b: %d\n",a < b);
+printf("a >= b: %d\n",a >= b);
+printf("a <= b: %d\n",a <= b);
+
+return 0;
+}
+  
