436ebd1c4d915ab5de4b3c73e130c4363b66a21e
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 18:04:28 +0530

    Create Pointer_Operator.c

---
+++ Programming for problem solving/chap1/Pointer_Operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10, b = 5, c;
+  int *ptr = &a;
+
+// Pointer Operator
+printf("Pointer Operator:\n");
+printf("Address of a: %p\n", &a);
+
+return 0;
+}
