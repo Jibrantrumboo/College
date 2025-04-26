c705118e5b620aba50cd8323348e55eea5ab9436
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:17:54 +0530

    Create Reminder_of_2nums.c

---
+++ Programming for problem solving/chap1/Reminder_of_2nums.c
@@ -0,0 +1,15 @@
+//For modulus operator, only int value can be used
+#include <stdio.h>
+int main(){
+  int a,b;
+  printf("\nInput the first number\n");
+  scanf("%d",&a);
+  printf("\nInput the second number\n");
+  scanf("%d",&b);
+  if(b!=0){
+      printf("\nThe reminder when %d is divided by %d is %d\n",a,b,a%b);}
+  else{
+      printf("\nThe division is not possible\n");}
+
+return 0;
+}
