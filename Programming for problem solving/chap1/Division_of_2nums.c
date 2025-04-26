66ca9b1ff82212780c72cba9e36c2262b91c00c5
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:26:33 +0530

    Create Division_of_2nums.c

---
+++ Programming for problem solving/chap1/Division_of_2nums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  float a,b;
+  printf("\nInput the first number\n");
+  scanf("%f",&a);
+  printf("\nInput the second number\n");
+  scanf("%f",&b);
+ if(b!=0){
+ printf("\nThe divison of %f and %f is %f,a,b,a/b);} 
+else{
+ printf("The divison is not possible");
+
+ return 0;
