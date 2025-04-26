3666aa1de643001d2b0c774a40cb3511bb6659c7
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:51:27 +0530

    Create Arithmetic_Operators.c

---
+++ Programming for problem solving/chap1/Arithmetic_Operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+  int a = 10, b = 5, c;
+  int *ptr = &a;
+
+//Arithmetic Operators
+printf("Arithmetic Operators:\n");
+printf("a + b = %d\n", a + b);
+printf("a - b = %d\n", a - b);
+printf("a * b = %d\n", a * b);
+printf("a / b = %d\n", a / b);
+printf("a %% b = %d\n\n", a % b);
+
+return 0;
+}
