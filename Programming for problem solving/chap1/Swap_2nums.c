52f1295207494f93a5e6230117d64bdb7c5e9451
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:40:49 +0530

    Create Swap_2nums.c

---
+++ Programming for problem solving/chap1/Swap_2nums.c
@@ -0,0 +1,16 @@
+#inlude <stdio.h>
+
+int main(){
+
+     float a,b,c,d;
+     printf("\nInput the first number\n");
+     scanf("%f",&a);
+     printf("nInput the second number\n");
+     scanf("%f",&b);
+     c=b;
+     d=a;
+     printf("%f %f",c,d);
+
+return 0;
+}
+    
