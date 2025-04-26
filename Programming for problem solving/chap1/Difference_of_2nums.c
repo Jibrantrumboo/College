636021a2c41faee35c3841a870fbff7429591aca
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:10:14 +0530

    Create Difference_of_2nums.c

---
+++ Programming for problem solving/chap1/Difference_of_2nums.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  float a,b;
+  printf("\nInput the first number\n");
+  scanf("%f,&a);
+  printf("\nInput the second number\n");
+  scanf("%f",&b);
+  printf("\nThe difference %f and %f is %f,a,b,a-b);
+
+return 0;
