7e8b499a82a3da424252d4e5df3c408145c85f86
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 16:32:14 +0530

    Create Product_of_2nums.c

---
+++ Programming for problem solving/chap1/Product_of_2nums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    float a,b;
+    printf("\nInput the first number\n");
+    scanf("%f",&a);
+    printf("\nInput the second number\n");
+    scanf("%f",&b);
+    printf("\nThe product of %f and %f is %f\n",a,b,a*b);
+
+return 0;
+}
