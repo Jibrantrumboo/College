9162d92d26723f622ed614de4e1f6a747a1ae20c
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 17:53:22 +0530

    Create Conditional(Ternary)_Operators.c

---
+++ Programming for problem solving/chap1/Conditional(Ternary)_Operators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int a = 10, b = 5, c =;
+  int *ptr = &a;
+
+// Conditional(Ternary)Operators
+printf("Conditional Operators:\n");
+int max = (a > b) ? a : b;
+printf("Max of a and b: %d\n\n", max);
+
+return 0;
+}
