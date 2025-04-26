45bf041c4151a8ee368acd1f02a255f5ba89a1a1
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 15:13:44 +0530

    Create Sum_of_2nums.c

---
+++ Programming for problem solving/chap1/Sum_of_2nums.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  float a,b;
+printf("\nInput the first number\n");
+scanf("%f",&a);
+printf("\nInput the second number\n");
+scanf("%f",&b);
+printf("nThe sum of %f and %f is %f,a,b,a+b);
+
+  return 0;
+}
