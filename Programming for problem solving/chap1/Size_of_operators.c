941ad100f4178ac04b7be6b3c1cf429ef341ddb6
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-04-26 18:00:34 +0530

    Create Size_of_operators.c

---
+++ Programming for problem solving/chap1/Size_of_operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10, b = 5, c;
+  int *ptr = &a;
+
+// size of Operator
+printf("size of Operator:\n");
+printf("size of int: %d bytes\n", sizeof(int));
+printf("size of float: %f bytes\n", sizeof(float));
+printf("size of char: %d bytes\n\n", sizeof(char));
+
+return 0;
+}
+
+
