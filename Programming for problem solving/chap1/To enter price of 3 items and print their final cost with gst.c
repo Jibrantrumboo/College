210ac2322f7dbd9aeb1c8af3388cd4f5ac22364b
Jibrantrumboo <jibrantrumboo@gmail.com>
2025-07-29 16:47:46 +0530

    Create To enter price of 3 items and print their final cost with gst.c

---
+++ Programming for problem solving/chap1/To enter price of 3 items and print their final cost with gst.c
@@ -0,0 +1,14 @@
+//Program to enter price of 3 items & print their final cost with gst
+
+#include<stdio.h>
+
+int main() {
+    float price[3];
+    printf("Enter three prices: ");
+    scanf("%f", &price[0]);
+    scanf("%f", &price[1]);
+    scanf("%f", &price[2]);
+
+    printf("Total price is: ");
+    return 0;
+}
